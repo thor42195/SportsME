594c4b2ee49fbb07ca82886fda1115c390871896
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-11-25 22:08:10 +0900

    modify redirect path while login and made search form size bigger

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,22 @@
 Rails.application.routes.draw do
-  root to: 'top#index'
 
   devise_for :users
+  authenticated :user do
+    root :to => "events#index"
+  end
+
+  authenticated :organizer do
+    root :to => "events#manage"
+  end
+
+  devise_for :organizers, controllers: {
+  sessions:      'organizers/sessions',
+  passwords:     'organizers/passwords',
+  registrations: 'organizers/registrations'
+  }
+
+  root to: 'top#index'
+
   resources :users do
     collection do
       post :confirm
@@ -12,11 +27,6 @@ Rails.application.routes.draw do
   get '/users/paticipants/:id', to:'users#paticipants'
   get '/users/followers/:id', to:'users#followers'
 
-  devise_for :organizers, controllers: {
-  sessions:      'organizers/sessions',
-  passwords:     'organizers/passwords',
-  registrations: 'organizers/registrations'
-  }
   resources :organizers do
     collection do
       post :confirm
@@ -29,15 +39,14 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :manage
-  get 'manage/events/:id', to:'events#manage'
 
+  get 'manage/events/:id', to:'events#manage'
   get 'search', to:'events#search'
 
   resources :interests, only: [:create, :destroy] # user can keep event as interests
   resources :bookmarks, only: [:create, :destroy]
   resources :paticipants, only: [:create, :destroy]
-  resources :reactions, only: [:index, :create]
+  resources :reactions, only: [:index, :create, :destroy]
   resources :chat, only: [:create, :show]
   resources :matching, only: %i(index)
 
