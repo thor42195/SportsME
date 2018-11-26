02986f87313d6666b9a07daf5242dfac15de8d73
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-11-26 20:42:10 +0900

    modify seed data

--- db/fixtures/production/5reaction.rb
+++ db/fixtures/production/5reaction.rb
@@ -1,7 +1,7 @@
 Reaction.seed do |s|
   s.id = 1
   s.follower_id = 7
-  s.following_id = 1
+  s.followed_id = 1
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -9,7 +9,7 @@ end
 Reaction.seed do |s|
   s.id = 2
   s.follower_id = 7
-  s.following_id = 2
+  s.followed_id = 2
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -18,7 +18,7 @@ end
 Reaction.seed do |s|
   s.id = 3
   s.follower_id = 7
-  s.following_id = 3
+  s.followed_id = 3
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -26,7 +26,7 @@ end
 Reaction.seed do |s|
   s.id = 4
   s.follower_id = 7
-  s.following_id = 4
+  s.followed_id = 4
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -34,7 +34,7 @@ end
 Reaction.seed do |s|
   s.id = 5
   s.follower_id = 7
-  s.following_id = 5
+  s.followed_id = 5
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -42,7 +42,7 @@ end
 Reaction.seed do |s|
   s.id = 6
   s.follower_id = 7
-  s.following_id = 6
+  s.followed_id = 6
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -50,7 +50,7 @@ end
 Reaction.seed do |s|
   s.id = 7
   s.follower_id = 1
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -58,7 +58,7 @@ end
 Reaction.seed do |s|
   s.id = 8
   s.follower_id = 2
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -67,7 +67,7 @@ end
 Reaction.seed do |s|
   s.id = 9
   s.follower_id = 3
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -75,7 +75,7 @@ end
 Reaction.seed do |s|
   s.id = 10
   s.follower_id = 4
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -83,7 +83,7 @@ end
 Reaction.seed do |s|
   s.id = 11
   s.follower_id = 5
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
@@ -91,7 +91,7 @@ end
 Reaction.seed do |s|
   s.id = 12
   s.follower_id = 6
-  s.following_id = 7
+  s.followed_id = 7
   s.created_at = "2018-10-10 00:20:00"
   s.updated_at = "2018-10-15 00:20:00"
 end
