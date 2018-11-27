c15f0c34bca8f60b7b3540c856248c98fdef9dd0
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-11-27 16:18:46 +0900

    change timezone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SportsME
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.time_zone = ‘Tokyo’ config.active_record.default_timezone = :local　
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
