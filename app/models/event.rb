66405adc4a6ccb7482ea08f1b7d0c1e5318ca0fd
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-11-28 04:44:17 +0900

    set validation and organizer cant set event day before current time, set enddate before start day and set due day before start and end day

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,4 +18,24 @@ class Event < ApplicationRecord
     %w(title place)
   end
 
+  validates :title, presence:true
+  validates :image, presence:true
+  validates :content, length:{in: 1..500}, presence:true
+
+  ## time validation
+  validate :not_before_today
+  validate :enddate_after_startdate
+  validate :duedate_is_first
+
+  def not_before_today
+    errors.add(:startdate, 'は今日以降の日付を設定してください') if startdate.nil? || startdate < Date.today
+  end
+
+  def enddate_after_startdate
+    errors.add(:enddate, 'は開始日よりも後の日程を設定してください') if enddate.nil? ||  enddate < startdate
+  end
+
+  def duedate_is_first
+    errors.add(:duedate, 'は開始日よりも前に設定してください') if duedate.nil? || startdate && enddate < duedate  
+  end
 end
