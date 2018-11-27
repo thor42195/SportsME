97fa099b6e7f36c0f5f318d9ecc7d7fa88ae6640
加瀬徹 <kasetooru@kasetoorunoMacBook.local>
2018-11-28 01:57:38 +0900

    set timezone

---
+++ db/fixtures/development/2user.rb
@@ -0,0 +1,91 @@
+User.seed do |s|
+  s.id = 1
+  s.name = "Cloud"
+  s.email = "cloud@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 0
+  s.introduction = "こんにちは、ランニングと筋トレが趣味です。"
+  s.sport = "オールラウンド"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+User.seed do |s|
+  s.id = 2
+  s.name = "Daniel"
+  s.email = "daniel@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 0
+  s.introduction = "こんにちは、最近サッカーを始めました。"
+  s.sport = "サッカーとか"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+User.seed do |s|
+  s.id = 3
+  s.name = "ゴーン"
+  s.email = "gone@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 0
+  s.introduction = "こんにちは、自転車が趣味でよくヒルクライムをします"
+  s.sport = "ロードバイク"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+
+User.seed do |s|
+  s.id = 4
+  s.name = "ハンナ"
+  s.email = "hanna@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 1
+  s.introduction = "こんにちは、自転車に興味があるので、一緒に乗ってくれる人を探しています。"
+  s.sport = "ロードバイク"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+User.seed do |s|
+  s.id = 5
+  s.name = "ナミゾウ"
+  s.email = "nami@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 1
+  s.introduction = "こんにちは、トライアスロンをやっています。"
+  s.sport = "ランニング・ロードバイク・スイミング"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+User.seed do |s|
+  s.id = 6
+  s.name = "花子"
+  s.email = "hanako@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 1
+  s.introduction = "こんにちは、ダイエットのために筋トレ始めました"
+  s.sport = "筋トレ"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+User.seed do |s|
+  s.id = 7
+  s.name = "test"
+  s.email = "test@user.com"
+  s.password = "testtest"
+  s.password_confirmation = "testtest"
+  s.sex = 1
+  s.introduction = "i like runnning"
+  s.sport = "筋トレ"
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
---
+++ db/fixtures/development/5reaction.rb
@@ -0,0 +1,102 @@
+=begin
+Reaction.seed do |s|
+  s.id = 1
+  s.follower_id = 7
+  s.followed_id = 1
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+=end
+
+Reaction.seed do |s|
+  s.id = 2
+  s.follower_id = 7
+  s.followed_id = 2
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+
+Reaction.seed do |s|
+  s.id = 3
+  s.follower_id = 7
+  s.followed_id = 3
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 4
+  s.follower_id = 7
+  s.followed_id = 4
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+=begin
+Reaction.seed do |s|
+  s.id = 5
+  s.follower_id = 7
+  s.followed_id = 5
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 6
+  s.follower_id = 7
+  s.followed_id = 6
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+=begin
+Reaction.seed do |s|
+  s.id = 7
+  s.follower_id = 1
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 8
+  s.follower_id = 2
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+
+Reaction.seed do |s|
+  s.id = 9
+  s.follower_id = 3
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 10
+  s.follower_id = 4
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 11
+  s.follower_id = 5
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+Reaction.seed do |s|
+  s.id = 12
+  s.follower_id = 6
+  s.followed_id = 7
+  s.created_at = "2018-10-10 00:20:00"
+  s.updated_at = "2018-10-15 00:20:00"
+end
+
+=end
